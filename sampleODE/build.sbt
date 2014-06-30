b31577b7dc31d0353220ff2d22cefacc3a8a5f24
William Waites <wwaites@tardis.ed.ac.uk>
2014-06-30 21:34:01 +0000

    almost working

--- sampleODE/build.sbt
+++ sampleODE/build.sbt
@@ -1,11 +1,11 @@
-name := "mois-examples"
+name := "SampleODE"
 
 organization := "uk.ac.ed.inf"
 
 version := "1.99.2-SNAPSHOT"
 
-scalaVersion := "2.10.4"
+scalaVersion := "2.11.1"
 
 resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
 
-libraryDependencies += "uk.ac.ed.inf" %% "mois" % "1.99.2-SNAPSHOT"
+libraryDependencies += "uk.ac.ed.inf" %% "mois-sbt" % "1.99.2-SNAPSHOT"
---
+++ sampleODE/src/main/scala/uk/ac/ed/inf/mois/examples/sample/SampleODE.scala
@@ -0,0 +1,22 @@
+package uk.ac.ed.inf.mois.examples.sample
+
+import uk.ac.ed.inf.mois.sbt.MoisMain
+import uk.ac.ed.inf.mois.{ProcessODE, State, Var}
+
+object sampleODE extends ProcessODE("sample") {
+  integral(
+    Var(25.0, "ex:x1"),
+    Var(50.0, "ex:x2")
+  )
+  def computeDerivatives(t: Double, y: Array[Double], ẏ: Array[Double]) {
+    ẏ(0) = -0.3*y(0) - 0.4*y(1)
+    ẏ(1) = -0.5*y(0) - 0.8*y(1)
+  }
+}
+
+final class SampleODEModel extends MoisMain {
+  val state = new State
+  state := Var(25.0, "ex:x1")
+  state := Var(50.0, "ex:x2")
+  val process = sampleODE
+}
