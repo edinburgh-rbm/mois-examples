9cd56bd68ee42ea73ebf021f798a05c8abb84d94
William Waites <wwaites@tardis.ed.ac.uk>
2014-07-12 12:38:06 +0000

    add main to skeletal example

--- skel/src/main/scala/org/example/Example.scala
+++ skel/src/main/scala/org/example/Example.scala
@@ -1,7 +1,11 @@
 package org.example
 
-import uk.ac.ed.inf.mois.Process
+import uk.ac.ed.inf.mois.{MoisMain, Process}
 
 class Example extends Process("example") {
   def step(t: Double, tau: Double) {}
+}
+
+object ExampleModel extends MoisMain("Example") {
+  val model = new Example
 }
