fee9dffba5973f824ebc346c86c1ccca1b2cf47b
Ricardo Honorato-Zimmer <rikardo.horo@gmail.com>
2014-07-25 19:18:09 +0100

    put some enzymatic reactions in Replication

--- src/main/scala/uk/ac/ed/inf/mois/examples/Replication.scala
+++ src/main/scala/uk/ac/ed/inf/mois/examples/Replication.scala
@@ -5,25 +5,25 @@ import uk.ac.ed.inf.mois.{DeterministicReactionNetwork, Model}
 class Replication
   extends DeterministicReactionNetwork("Brusselator") {
   
-  val ATP = Specie("ATP")
-  val H20 = Specie("H2O")
-  val ADP = Specie("ADP")
-  val H = Specie("H")
-  val PI = Specie("PI")
+  val ATP = Species("ATP")
+  val H20 = Species("H2O")
+  val ADP = Species("ADP")
+  val H = Species("H")
+  val PI = Species("PI")
   val DR5P = Species("DR5P")
   val NAD = Species("NAD")
   val AMP = Species("AMP")
-  val dRibose5P_dRibose5P = Species("dRibose5P_dRibose5P") 
+  val dRibose5P_dRibose5P = Species("dRibose5P_dRibose5P")
   val NMN = Species("NMN")
-  val DAMP = Species("DAMP") 
+  val DAMP = Species("DAMP")
   val DATP = Species("DATP")
-  val dApdAp = Species("dApdAp") 
+  val dApdAp = Species("dApdAp")
   val PPI = Species("PPI")
-  val DCMP = Species("DCMP") 
+  val DCMP = Species("DCMP")
   val DCTP = Species("DCTP")
-  val dCpdCp = Species("dCpdCp") 
+  val dCpdCp = Species("dCpdCp")
   val PPI = Species("PPI")
-  val DGMP = Species("DGMP") 
+  val DGMP = Species("DGMP")
   val DGTP = Species("DGTP")
   val dGpdGp = Species("dGpdGp")
   val DTMP = Species("DTMP")
@@ -55,58 +55,123 @@ class Replication
   val MG_091_OCTAMER = Species("MG_091_OCTAMER")
   val MG_094_HEXAMER = Species("MG_094_HEXAMER")
   val REPLISOME = Species("REPLISOME")
-  
+
   reactions(
-// DNA unwinding
+    // -- Chemical reactions --
+
+    // DNA unwinding
     ATP + H2O -> ADP + H + PI at 1.0,
-// DNA ligationi
-    (2) DR5P + NAD ⇒ AMP + dRibose5P_dRibose5P + H + NMN
-// DNA polymerization (dATP)
+    // DNA ligation
+    2 * DR5P + NAD -> AMP + dRibose5P_dRibose5P + H + NMN
+      catalysedBy MG_254_MONOMER using QSS(vmax = 2.4, // 1/min
+                                           km = 4.1), // nM
+    // DNA polymerization (dATP)
     DAMP + DATP -> dApdAp + PPI
-// DNA polymerization (dCTP)
+      catalysedBy DNA_POLYMERASE_CORE using QSS(vmax = 6000, // 1/min
+                                                km = 4.1), // nM
+    // DNA polymerization (dCTP)
     DCMP + DCTP -> dCpdCp + PPI
-// DNA polymerization (dGTP)
+      catalysedBy DNA_POLYMERASE_CORE using QSS(vmax = 6000, // 1/min
+                                                km = 4.1), // nM
+    // DNA polymerization (dGTP)
     DGMP + DGTP -> dGpdGp + PPI
-// DNA polymerization (dTTP)
+      catalysedBy DNA_POLYMERASE_CORE using QSS(vmax = 6000, // 1/min
+                                                km = 4.1), // nM
+    // DNA polymerization (dTTP)
     DTMP + DTTP -> dTpdTp + PPI
-// DNA primer polymerization (ATP)
+      catalysedBy DNA_POLYMERASE_CORE using QSS(vmax = 6000, // 1/min
+                                                km = 4.1), // nM
+    // DNA primer polymerization (ATP)
     AMP + ATP -> ApAp + PPI
-// DNA primer polymerization (CTP)
+      catalysedBy MG_250_MONOMER using QSS(vmax = 6000, // 1/min
+                                           km = 3e5), // nM
+    // DNA primer polymerization (CTP)
     CMP + CTP -> CpCp + PPI
-// DNA primer polymerization (GTP)
+      catalysedBy MG_250_MONOMER using QSS(vmax = 6000, // 1/min
+                                           km = 3e5), // nM
+    // DNA primer polymerization (GTP)
     GMP + GTP -> GpGp + PPI
-// DNA primer polymerization (UTP)
+      catalysedBy MG_250_MONOMER using QSS(vmax = 6000, // 1/min
+                                           km = 3e5), // nM
+    // DNA primer polymerization (UTP)
     UMP + UTP -> PPI + UpUp
+      catalysedBy MG_250_MONOMER using QSS(vmax = 6000, // 1/min
+                                           km = 3e5), // nM
 
-//// Now the complex formation reactions
+    // -- Complex formation reactions --
 
-//    DNA polymerase (2) core, β-clamp, γ-complex, and primase
-    (2) DNA_POLYMERASE_CORE + DNA_POLYMERASE_GAMMA_COMPLEX + (2) MG_001_MONOMER + MG_250_MONOMER -> DNA_POLYMERASE_2CORE_BETA_CLAMP_GAMMA_COMPLEX_PRIMASE
-//    DNA polymerase core, β-clamp, and γ-complex
-    DNA_POLYMERASE_CORE + DNA_POLYMERASE_GAMMA_COMPLEX + (2) MG_001_MONOMER -> DNA_POLYMERASE_CORE_BETA_CLAMP_GAMMA_COMPLEX
-//    DNA polymerase core, β-clamp, and primase  
-    DNA_POLYMERASE_CORE + (2) MG_001_MONOMER + MG_250_MONOMER -> DNA_POLYMERASE_CORE_BETA_CLAMP_PRIMASE
-//    DNA-directed DNA polymerase holoenzyme
-    (2) DNA_POLYMERASE_CORE + DNA_POLYMERASE_GAMMA_COMPLEX + (2) MG_001_MONOMER -> DNA_POLYMERASE_HOLOENZYME
-//    DNA polymerase III, beta clamp
-    (2) MG_001_MONOMER -> MG_001_DIMER
-//    single-strand binding protein family, octamer
-    (2) MG_091_TETRAMER -> MG_091_OCTAMER
-//    Replisome
-    (2) DNA_POLYMERASE_CORE + DNA_POLYMERASE_GAMMA_COMPLEX + (4) MG_001_MONOMER + MG_094_HEXAMER + MG_250_MONOMER -> REPLISOME
-
+    // DNA polymerase (2) core, β-clamp, γ-complex, and primase
+    2 * DNA_POLYMERASE_CORE + DNA_POLYMERASE_GAMMA_COMPLEX + 2 * MG_001_MONOMER + MG_250_MONOMER ->
+      DNA_POLYMERASE_2CORE_BETA_CLAMP_GAMMA_COMPLEX_PRIMASE,
+    // DNA polymerase core, β-clamp, and γ-complex
+    DNA_POLYMERASE_CORE + DNA_POLYMERASE_GAMMA_COMPLEX + 2 * MG_001_MONOMER -> DNA_POLYMERASE_CORE_BETA_CLAMP_GAMMA_COMPLEX,
+    // DNA polymerase core, β-clamp, and primase
+    DNA_POLYMERASE_CORE + 2 * MG_001_MONOMER + MG_250_MONOMER -> DNA_POLYMERASE_CORE_BETA_CLAMP_PRIMASE,
+    // DNA-directed DNA polymerase holoenzyme
+    2 * DNA_POLYMERASE_CORE + DNA_POLYMERASE_GAMMA_COMPLEX + 2 * MG_001_MONOMER -> DNA_POLYMERASE_HOLOENZYME,
+    // DNA polymerase III, beta clamp
+    2 * MG_001_MONOMER -> MG_001_DIMER,
+    // Single-strand binding protein family, octamer
+    2 * MG_091_TETRAMER -> MG_091_OCTAMER,
+    // Replisome
+    2 * DNA_POLYMERASE_CORE + DNA_POLYMERASE_GAMMA_COMPLEX + 4 * MG_001_MONOMER + MG_094_HEXAMER + MG_250_MONOMER -> REPLISOME
   )
 }
 
-/**
- * BrusselatorModel is used generically in test:run because it is a
- * decent model to test things like graphing out.
- */
-class BrusselatorModel extends Model {
-  val process = new Brusselator
+/** ReplicationModel is used generically in test:run because it is a
+  * decent model to test things like graphing out.
+  */
+class ReplicationModel extends Model {
+  val process = new Replication
   import process._
-  A := 1.0
-  B := 1.7
-  X := 1.0
-  Y := 1.0
+  ATP := 1.0
+  H20 := 1.0
+  ADP := 1.0
+  H := 1.0
+  PI := 1.0
+  DR5P := 1.0
+  NAD := 1.0
+  AMP := 1.0
+  dRibose5P_dRibose5P := 1.0
+  NMN := 1.0
+  DAMP := 1.0
+  DATP := 1.0
+  dApdAp := 1.0
+  PPI := 1.0
+  DCMP := 1.0
+  DCTP := 1.0
+  dCpdCp := 1.0
+  PPI := 1.0
+  DGMP := 1.0
+  DGTP := 1.0
+  dGpdGp := 1.0
+  DTMP := 1.0
+  DTTP := 1.0
+  dTpdTp := 1.0
+  ApAp := 1.0
+  CMP := 1.0
+  CTP := 1.0
+  CpCp := 1.0
+  GMP := 1.0
+  GTP := 1.0
+  GpGp := 1.0
+  UMP := 1.0
+  UTP := 1.0
+  UpUp := 1.0
+
+  DNA_POLYMERASE_CORE := 1.0
+  DNA_POLYMERASE_GAMMA_COMPLEX := 1.0
+  MG_001_MONOMER := 1.0
+  MG_250_MONOMER := 1.0
+  DNA_POLYMERASE_2CORE_BETA_CLAMP_GAMMA_COMPLEX_PRIMASE := 1.0
+  DNA_POLYMERASE_CORE := 1.0
+  DNA_POLYMERASE_GAMMA_COMPLEX := 1.0
+  DNA_POLYMERASE_CORE_BETA_CLAMP_GAMMA_COMPLEX := 1.0
+  DNA_POLYMERASE_CORE_BETA_CLAMP_PRIMASE := 1.0
+  DNA_POLYMERASE_HOLOENZYME := 1.0
+  MG_001_DIMER := 1.0
+  MG_091_TETRAMER := 1.0
+  MG_091_OCTAMER := 1.0
+  MG_094_HEXAMER := 1.0
+  REPLISOME := 1.0
 }
