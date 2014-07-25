6294d1753cfcaa40fce1fcda1e09ff77c984b8f8
William Waites <wwaites@tardis.ed.ac.uk>
2014-07-25 15:20:29 +0100

    add brusselator example

---
+++ src/main/scala/uk/ac/ed/inf/mois/examples/Brusselator.scala
@@ -0,0 +1,32 @@
+package uk.ac.ed.inf.mois.examples
+
+import uk.ac.ed.inf.mois.{DeterministicReactionNetwork, Model}
+
+class Brusselator
+  extends DeterministicReactionNetwork("Brusselator") {
+  
+  val A = Specie("A")
+  val B = Specie("B")
+  val X = Specie("X")
+  val Y = Specie("Y")
+  
+  reactions(
+    A -> X + A at 1.0,
+    2(X) + Y -> 3(X) at 1.0,
+    B + X -> B + Y at 1.0,
+    X -> () at 1.0
+  )
+}
+
+/**
+ * BrusselatorModel is used generically in test:run because it is a
+ * decent model to test things like graphing out.
+ */
+class BrusselatorModel extends Model {
+  val process = new Brusselator
+  import process._
+  A := 1.0
+  B := 1.7
+  X := 1.0
+  Y := 1.0
+}
